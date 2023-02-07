a20c7f4acafcfe1f94f33cf941e9a8f75aee26c1
pawanjaatpk <118508835+pawanjaatpk@users.noreply.github.com>
2023-02-07 23:29:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/pawanjaatpk/Channel-Mdisk-Converter.git /Channel-Mdisk-Converter
+  git clone https://github.com/pawanjaatpk/Mdiskop.git /Mdiskop
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Channel-Mdisk-Converter
+  git clone $SOURCE_CODE /Mdiskop
 fi
-cd /Channel-Mdisk-Converter
+cd /Mdiskop
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
